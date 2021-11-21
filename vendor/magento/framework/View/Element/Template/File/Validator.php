007a1ac5374eef0eb9244bcf97d42cc320a02443
thayvovi <tung140520@gmail.com>
2021-11-21 16:26:51 +0700

    edit realPath in Validator.php

--- vendor/magento/framework/View/Element/Template/File/Validator.php
+++ vendor/magento/framework/View/Element/Template/File/Validator.php
@@ -135,7 +135,8 @@ class Validator
         if (!is_array($directories)) {
             $directories = (array)$directories;
         }
-        $realPath = $this->fileDriver->getRealPath($path);
+        // $realPath = $this->fileDriver->getRealPath($path);
+        $realPath = str_replace('\\', '/', $this->fileDriver->getRealPath($path));
         foreach ($directories as $directory) {
             if (0 === strpos($realPath, $directory)) {
                 return true;
